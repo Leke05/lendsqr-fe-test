6287e7de3214bd005e381e4b00f462b37fee0d9e
I am God <babatundejonathan05@gmail.com>
2024-06-28 05:02:07 +0100

    fix api issue

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,6 +1,6 @@
 export const lendsqr = {
   users: {
-    getUserInfo: "28adcd44-05fa-48ae-bca6-959b6776838c",
+    getUserInfo: "ef1d5279-3683-4930-b602-e4b56dcbae0b",
     getUserStatus: "1c707f19-3a1a-4a1a-bd0e-09ffffdacca4",
     getUserSidebar: "2375f699-9fda-4b72-89ac-cc8efd61b114",
   },
